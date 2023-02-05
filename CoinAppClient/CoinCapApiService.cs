8c8bb75b5d7d57f6aa9cbd6fd94bbe8d78b32a20
Volodymyr Hula <volodia.gula@ukr.net>
2023-02-05 11:14:02 +0200

    Add Api service

---
+++ CoinAppClient/CoinCapApiService.cs
@@ -0,0 +1,38 @@
+﻿namespace CoinAppClient;
+
+/// <summary>
+/// Provides a class for sending HTTP requests and receiving HTTP responses
+/// </summary>
+internal class CoinCapApiService
+{
+    private readonly HttpClient _http;
+
+    public CoinCapApiService()
+    {
+        _http = new HttpClient();
+    }
+
+    /// <summary>
+    /// Sedns HTTP GET request to the given URL
+    /// </summary>
+    /// <param name="url">URL to request</param>
+    /// <returns>Response message as HttpResponseMessage object</returns>
+    /// <exception cref="ArgumentNullException">If given <paramref name="url"/> is null</exception>
+    /// <exception cref="ArgumentException">if request was unsuccessful</exception>
+    public Task<HttpResponseMessage> GetRequest(string? url)
+    {
+        if (string.IsNullOrEmpty(url?.Trim()))
+            throw new ArgumentNullException(nameof(url));
+
+        return GetRequestAsync(url);
+
+        async Task<HttpResponseMessage> GetRequestAsync(string urlAddress)
+        {
+            var response = await _http.GetAsync(urlAddress);
+            if (response is null)
+                throw new ArgumentException($"Bad request to url:{urlAddress}");
+
+            return response;
+        }
+    }
+}

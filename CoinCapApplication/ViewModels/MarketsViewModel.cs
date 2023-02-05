5fd99d6307e8f76207d77613b13b1824bab46aec
Volodymyr Hula <volodia.gula@ukr.net>
2023-02-05 13:39:04 +0200

    Added markets view model

---
+++ CoinCapApplication/ViewModels/MarketsViewModel.cs
@@ -0,0 +1,57 @@
+﻿using CoinAppClient;
+using CoinAppClient.Models;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+
+namespace CoinCapApplication.ViewModels;
+internal class MarketsViewModel : BaseViewModel
+{
+    private readonly CoinCapService _coinCap;
+    private ObservableCollection<ApiExchange> _exchanges;
+
+    public ObservableCollection<ApiExchange> Exchanges
+    {
+        get => _exchanges;
+        private set { 
+            _exchanges = value;
+            OnPropertyChanged(nameof(Exchanges));
+        }
+    }
+
+    private ObservableCollection<ApiMarkets> _selectedMarket;
+    public ObservableCollection<ApiMarkets> SelectedMarket
+    {
+        get => _selectedMarket;
+        set
+        {
+            _selectedMarket = value;
+            OnPropertyChanged(nameof(SelectedMarket));
+        }
+    }
+
+    private ObservableCollection<ApiMarkets> _markets;
+    public ObservableCollection<ApiMarkets> Markets
+    {
+        get => _markets;
+        private set
+        {
+            _markets = value;
+            OnPropertyChanged(nameof(Markets));
+        }
+    }
+
+    public MarketsViewModel()
+    {
+        _coinCap = new CoinCapService();
+        LoadData();
+    }
+
+    private async Task LoadData()
+    {
+        var apiExchanges = await _coinCap.GetApiExchanges();
+        var apiMarkets = await _coinCap.GetApiMarkets();
+        Exchanges = new ObservableCollection<ApiExchange>(apiExchanges);
+        Markets = new ObservableCollection<ApiMarkets>(apiMarkets);
+    }
+}

2605b5af871e004cb355e1f1303b4ad2c89d8972
Volodymyr Hula <volodia.gula@ukr.net>
2023-02-05 11:14:56 +0200

    Add service for handling HTTP responses

---
+++ CoinAppClient/CoinCapService.cs
@@ -0,0 +1,46 @@
+﻿using CoinAppClient.Models;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+
+namespace CoinAppClient;
+public class CoinCapService
+{
+    private readonly CoinCapApiService _apiService;
+    private const string _baseAddress = "https://api.coincap.io/v2/";
+
+    public CoinCapService()
+    {
+        _apiService = new CoinCapApiService();
+    }
+
+    public async Task<List<ApiMarkets>> GetApiMarkets()
+    {
+        var response = await _apiService.GetRequest(_baseAddress + "markets");
+        var apiMarkets = await GetApiData<List<ApiMarkets>>(response);
+
+        return apiMarkets is null ? new List<ApiMarkets>() : apiMarkets;
+    }
+
+
+    /// <summary>
+    /// Converts data from HTTP response into given type parameter <typeparamref name="M"/>
+    /// </summary>
+    /// <typeparam name="M">Type to convert data</typeparam>
+    /// <param name="responseMessage">HTTP response</param>
+    /// <returns>Converted data if the parsing was successful, otherwise null</returns>
+    private async Task<M?> GetApiData<M>(HttpResponseMessage responseMessage) where M: class
+    {
+        string stringResponse = await responseMessage.Content.ReadAsStringAsync();
+        try
+        {
+            var JsonArray = JsonNode.Parse(stringResponse)?["data"];
+            var model = JsonArray.Deserialize<M>(new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+            return model;
+        }
+        catch
+        {
+            return null;
+        }
+    }
+}

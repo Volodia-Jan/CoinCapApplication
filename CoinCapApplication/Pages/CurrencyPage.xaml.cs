6b20b3abc4c5808d5c3c0eda87fde123b3bb743f
Volodymyr Hula <volodia.gula@ukr.net>
2023-02-06 15:58:12 +0200

    Added currency page

---
+++ CoinCapApplication/Pages/CurrencyPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using CoinCapApplication.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CoinCapApplication.Pages;
+/// <summary>
+/// Interaction logic for CurrencyPage.xaml
+/// </summary>
+public partial class CurrencyPage : Page
+{
+    public CurrencyPage(string assetId)
+    {
+        InitializeComponent();
+        DataContext = new CurrencyViewModel(assetId);
+    }
+
+    private void GoBack_ButtonClick(object sender, RoutedEventArgs e)
+    {
+        NavigationService.GoBack();
+    }
+}

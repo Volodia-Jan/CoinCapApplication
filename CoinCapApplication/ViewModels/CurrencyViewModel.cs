aae2c857f329c178ccb13aca190d7ef535e557d9
Volodymyr Hula <volodia.gula@ukr.net>
2023-02-06 15:57:56 +0200

    Added currency view model

---
+++ CoinCapApplication/ViewModels/CurrencyViewModel.cs
@@ -0,0 +1,35 @@
+﻿using CoinAppClient;
+using CoinAppClient.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoinCapApplication.ViewModels;
+internal class CurrencyViewModel : BaseViewModel
+{
+    private readonly CoinCapService _coinCap;
+    private ApiAsset _asset;
+
+    public ApiAsset Asset
+    {
+        get => _asset;
+        private set
+        {
+            _asset = value;
+            OnPropertyChanged(nameof(Asset));
+        }
+    }
+
+    public CurrencyViewModel(string currencyId)
+    {
+        _coinCap = new CoinCapService();
+        LoadData(currencyId);
+    }
+
+    private async Task LoadData(string currencyId)
+    {
+        Asset = await _coinCap.GetAssetById(currencyId);
+    }
+}

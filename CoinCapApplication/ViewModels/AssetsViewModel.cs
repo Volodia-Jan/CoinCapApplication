6fc11890ccf46a7a1924e1a6f82e18b600eecc8d
Volodymyr Hula <volodia.gula@ukr.net>
2023-02-06 15:57:21 +0200

    Added assets view model

---
+++ CoinCapApplication/ViewModels/AssetsViewModel.cs
@@ -0,0 +1,37 @@
+﻿using CoinAppClient;
+using CoinAppClient.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoinCapApplication.ViewModels;
+internal class AssetsViewModel : BaseViewModel
+{
+    private readonly CoinCapService _coinCap;
+    private ObservableCollection<ApiAsset> _assets;
+    
+    public ObservableCollection<ApiAsset> Assets
+    {
+        get => _assets;
+        set
+        {
+            _assets = value;
+            OnPropertyChanged(nameof(Assets));
+        }
+    }
+
+    public AssetsViewModel()
+    {
+        _coinCap = new CoinCapService();
+        LoadData();
+    }
+
+    private async Task LoadData()
+    {
+        var assetsList = await _coinCap.GetApiAssets();
+        Assets = new ObservableCollection<ApiAsset>(assetsList);
+    }
+}

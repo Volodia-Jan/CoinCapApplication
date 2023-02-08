9b0a1056c12c556adce1972c6b0d02b6592ceb86
Volodymyr Hula <volodia.gula@ukr.net>
2023-02-08 14:48:29 +0200

    Change style of main window

--- CoinCapApplication/MainWindow.xaml.cs
+++ CoinCapApplication/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using CoinCapApplication.Pages;
 using System.Windows;
+using System.Windows.Input;
 
 namespace CoinCapApplication;
 /// <summary>
@@ -13,13 +14,18 @@ public partial class MainWindow : Window
         PageFrame.Content = new MarketsPage();
     }
 
-    private void MarketsPageNavigation(object sender, RoutedEventArgs e)
+    private void MarketsPageNavigation(object sender, MouseButtonEventArgs e)
     {
         PageFrame.Content = new MarketsPage();
     }
 
-    private void AssetsPageNavigation(object sender, RoutedEventArgs e)
+    private void AssetsPageNavigation(object sender, MouseButtonEventArgs e)
     {
         PageFrame.Content = new AssetsPage();
+    }
+
+    private void CloseBtnClick(object sender, RoutedEventArgs e)
+    {
+        Application.Current.Shutdown();
     }
 }
